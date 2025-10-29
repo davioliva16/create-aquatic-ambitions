7a3ac09f423a2f9617fee9b85a8de330df25f832
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-10-29 16:43:59 -0400

    Move to non-deprecated method

--- src/main/java/net/davio/aquaticambitions/registry/CAAItems.java
+++ src/main/java/net/davio/aquaticambitions/registry/CAAItems.java
@@ -1,11 +1,12 @@
 package net.davio.aquaticambitions.registry;
 
 import com.tterrag.registrate.util.entry.ItemEntry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
 import static com.simibubi.create.AllTags.AllItemTags.CREATE_INGOTS;
-import static com.simibubi.create.AllTags.commonItemTag;
 import static net.davio.aquaticambitions.CreateAquaticAmbitions.REGISTRATE;
 
 public class CAAItems {
@@ -16,7 +17,7 @@ public class CAAItems {
 
     public static final ItemEntry<Item>
         PRISMARINE_ALLOY = taggedIngredient("prismarine_alloy", CREATE_INGOTS.tag),
-        PRISMARINE_ROD = taggedIngredient("prismarine_alloy_rod",commonItemTag("rods")),
+        PRISMARINE_ROD = taggedIngredient("prismarine_alloy_rod", commonItemTag("rods")),
         CALCIUM_RICH_POWDER = ingredient("calcium_rich_powder"),
         SPIKY_SHELL = ingredient("spiky_shell"),
         SUSPICIOUS_ROCK = ingredient("suspicious_rock");
@@ -30,6 +31,10 @@ public class CAAItems {
     private static ItemEntry<Item> ingredient(String name){
         return REGISTRATE.item(name, Item::new)
                 .register();
+    }
+
+    private static TagKey<Item> commonItemTag(String key) {
+        return TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("c", key));
     }
 
     public static void register() {};
