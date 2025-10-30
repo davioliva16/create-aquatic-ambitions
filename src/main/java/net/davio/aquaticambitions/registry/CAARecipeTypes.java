19839bf863f8dc34ac828f68efd33806ee4f251a
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-10-29 20:06:10 -0400

    Don't complain about being unchecked in CAARecipeTypes

--- src/main/java/net/davio/aquaticambitions/registry/CAARecipeTypes.java
+++ src/main/java/net/davio/aquaticambitions/registry/CAARecipeTypes.java
@@ -22,6 +22,7 @@ import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+@SuppressWarnings("unchecked")
 public enum CAARecipeTypes implements IRecipeTypeInfo, StringRepresentable {
     CHANNELING(ChannelingRecipe::new);
 
